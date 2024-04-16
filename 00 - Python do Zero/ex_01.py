a0adf6fc7b7a2235e33e30a2d8f06172a314abb6
Alex Felipe Barbosa <alexfelipebarbosa@yahoo.com.br>
2024-04-15 21:14:01 -0300

    Introdução

---
+++ 00 - Python do Zero/ex_01.py
@@ -0,0 +1,18 @@
+nome_produto: str = 'Mesa de Escritório'
+quantidade_estoque: int = 10
+preco_unitario: float = 450.0
+disponivel_venda: bool = True
+data_validade = 2024-12-31
+
+#print("Olá mundo!")
+# print(quantidade_estoque)
+# print(f"Meu nome do Produto é: {nome_produto}")
+
+# nome_usuario: str = input(" Digite seu Nome: ")
+# print(nome_usuario)
+
+print(type(data_validade))
+print(data_validade)
+
+
+
---
+++ 00 - Python do Zero/ex_02.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+nome_produto: str = 'Mesa de Escritório'
+quantidade_estoque: int = 10
+preco_unitario: float = 450.0
+disponivel_venda: bool = True
+data_validade: datetime = datetime(2024,12,31)
+
+print(data_validade)
+print(type(data_validade))
+
+
+for dia in range(1,11): # Criar o dia [1,2,3,4,5,6,7,8,9,10]
+  print(f" Dia: {dia}")
+  quantidade_estoque = quantidade_estoque -1
+  print(f"Quantidade de estoque: {quantidade_estoque}")
+  
+  
+  if quantidade_estoque <= 5:
+    print('Estoque BAIXO! - Comprar mais Estoque')
+  else:
+    print('Estoque suficiente!')
+    
+print('Esse bloco')
